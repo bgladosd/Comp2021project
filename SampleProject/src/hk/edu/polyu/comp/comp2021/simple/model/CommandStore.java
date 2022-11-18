0867bba669454eb8fd8d9c08ea3ab7bfbe7f0d6f
bgladosd <bcavejohnsond@gmail.com>
2022-11-19 01:15:19 +0800

    merged the new codes of store Tidy it up

--- SampleProject/src/hk/edu/polyu/comp/comp2021/simple/model/CommandStore.java
+++ SampleProject/src/hk/edu/polyu/comp/comp2021/simple/model/CommandStore.java
@@ -3,93 +3,63 @@ package hk.edu.polyu.comp.comp2021.simple.model;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
 //run before program load
-public class CommandStore implements Command{
+public class CommandStore implements Command {
 
     String path;
     String progName;
     String label;
 
-
     @Override
     public DataObject execute(Memory m) {
         Command c = m.getProgram(progName);
-        if (c==null) {
-            System.out.println("Program : "+ progName +" can not be found ");
+        if (c == null) {
+            System.out.println("Program : " + progName + " can not be found ");
             return null;
         }
         m.reSetVariableData();
         m.resetRunnedCommand();
         m.setExecuting(false);
         c.execute(m);
-        
 
-
-        //print out all the runned command
+        // print out all the runned command
         while (!m.getRunnedCommand().isEmpty()) {
             Command cs = m.getRunnedCommand().remove();
             System.out.println(cs.getCmdString());
         }
-        
+
         try {
-            File store = new File(path+"/"+progName+".txt");
+            File store = new File(path + "/" + progName + ".txt");
             if (store.createNewFile()) {
-                //if no such txt file, create a new file
+                // if no such txt file, create a new file
 
                 try {
-                    FileWriter txtWriter = new FileWriter(path+"/"+progName+".txt");
-
-                    Command e = m.getProgram(progName);
-                    if (e==null) {
-                        System.out.println("Program : "+ progName +" can not be found ");
-                        return null;
-                    }
-                    m.SaveState();
-                    m.resetRunnedCommand();
-                    m.setExecuting(true);
-                    c.execute(m);
-                    m.setExecuting(false);
-
-
-                    //print out all the runned command
+                    FileWriter txtWriter = new FileWriter(path + "/" + progName + ".txt");
+                    // print out all the runned command
                     while (!m.getRunnedCommand().isEmpty()) {
                         Command cs = m.getRunnedCommand().remove();
-                        txtWriter.write(cs.getCmdString()+"\n");
+                        txtWriter.write(cs.getCmdString() + "\n");
                     }
-                    txtWriter.write("execute "+progName+"\n");
+                    txtWriter.write("execute " + progName + "\n");
                     txtWriter.close();
                 } catch (IOException e) {
                     System.out.println("Error: Failed to writing txt file.");
                     e.printStackTrace();
                 }
 
-                System.out.println(store.getName()+ " created");
+                System.out.println(store.getName() + " created");
             } else {
-                //if have such txt file, rewrite it
+                // if have such txt file, rewrite it
 
                 try {
-                    FileWriter txtWriter = new FileWriter(path+"/"+progName+".txt");
-
-
-                    Command d = m.getProgram(progName);
-                    if (d==null) {
-                        System.out.println("Program : "+ progName +" can not be found ");
-                        return null;
-                    }
-                    m.SaveState();
-                    m.resetRunnedCommand();
-                    m.setExecuting(true);
-                    c.execute(m);
-                    m.setExecuting(false);
-
-
-                    //print out all the runned command
+                    FileWriter txtWriter = new FileWriter(path + "/" + progName + ".txt");
+                    // print out all the runned command
                     while (!m.getRunnedCommand().isEmpty()) {
                         Command cs = m.getRunnedCommand().remove();
-                        txtWriter.write(cs.getCmdString()+"\n");
+                        txtWriter.write(cs.getCmdString() + "\n");
                     }
-                    txtWriter.write("execute "+progName+"\n");
-
+                    txtWriter.write("execute " + progName + "\n");
                     txtWriter.close();
                 } catch (IOException e) {
                     System.out.println("Error: Failed to writing txt file.");
@@ -101,22 +71,22 @@ public class CommandStore implements Command{
         } catch (IOException e) {
             System.out.println("Error: Failed to creating txt file.");
             e.printStackTrace();
-        }finally{
+        } finally {
 
         }
 
         return null;
     }
 
-    public CommandStore(String[] cmd, Memory m){
-        if(cmd.length!=3){            //store printeven C:\\
+    public CommandStore(String[] cmd, Memory m) {
+        if (cmd.length != 3) { // store printeven C:\\
             System.out.println(
                     "instruction failed! store statement should only have 3 elements which is (store programName path)");
             return;
         }
 
-        this.progName=cmd[1];
-        this.path=cmd[2];
+        this.progName = cmd[1];
+        this.path = cmd[2];
 
         this.execute(m);
 
@@ -124,13 +94,14 @@ public class CommandStore implements Command{
 
     @Override
     public void setLabel(String l) {
-        this.label=l;
+        this.label = l;
     }
 
     @Override
     public String getLabel() {
         return this.label;
     }
+
     @Override
     public String getCmdString() {
         return "";
@@ -138,6 +109,6 @@ public class CommandStore implements Command{
 
     @Override
     public void setCmdString(String s) {
-        
+
     }
 }
