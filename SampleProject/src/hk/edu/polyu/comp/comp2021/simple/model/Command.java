f478caf035e9b5b5c63ea55270cf3fb19a519bb1
bgladosd <bcavejohnsond@gmail.com>
2022-11-19 21:45:01 +0800

    commandcomment

--- SampleProject/src/hk/edu/polyu/comp/comp2021/simple/model/Command.java
+++ SampleProject/src/hk/edu/polyu/comp/comp2021/simple/model/Command.java
@@ -1,15 +1,30 @@
 package hk.edu.polyu.comp.comp2021.simple.model;
 
+/**
+ *  Command interface, a command should be able to execute get set label and get set the String input that build this command
+ */
 public interface Command {
-
+	/**
+	 * @param m access to memory
+	 * @return DataObject expression Command with return expression as dataobject
+	 */
 	public DataObject execute(Memory m);
 
+	/**
+	 * @param l set the label
+	 */
 	public void setLabel(String l);
-
+	/**
+	 * @return get the command label
+	 */
 	public String getLabel();
-
+	/**
+	 * @return get the string input that build the command
+	 */
 	public String getCmdString();
-
+	/** set command cmdString to string that build the command
+	 * @param s string that build the command
+	 */
 	public void setCmdString(String s);
 
 }
